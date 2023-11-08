7726cd6de708d0adb1884078993d4b49c9cc82bb
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2023-11-08 23:18:43 +0200

    start implementing charts

---
+++ src/components/ChartBox/ChartBox.tsx
@@ -0,0 +1,78 @@
+import { Link } from "react-router-dom";
+import "./style.scss";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+type Props = {
+  color: string,
+  icon: string,
+  title: string,
+  dataKey: string,
+  number: number | string,
+  percentage: number,
+  chartData: object[],
+}
+
+const ChartBox = (props: Props) => {
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={props.icon} alt="user" />
+          <span>{props.title}</span>
+        </div>
+        <h1>{props.number}</h1>
+        <Link to="/" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                }}
+                labelStyle={{
+                  display: "none",
+                }}
+                position={{
+                  x: 30,
+                  y: 120,
+                }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartBox;
